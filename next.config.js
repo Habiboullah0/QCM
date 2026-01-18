c322127b20148cdcfa535f8f052fe38c2a4bedab
Habiboullah0 <habiboullahmedlemin7@gmail.com>
2026-01-18 16:41:47 +0000

    new version fix

--- next.config.js
+++ next.config.js
@@ -2,11 +2,17 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
+  disable: process.env.NODE_ENV === 'development',
 });
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // 🔴 REQUIRED: next-pwa uses webpack
+  experimental: {
+    turbo: false,
+  },
 };
 
 module.exports = withPWA(nextConfig);
